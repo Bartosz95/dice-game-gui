d2d737a43554d0c8b0970b7ec6d6d5f6247e2390
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-04-02 01:06:04 +0200

    Add fetching data from API

--- src/components/dice/dice.js
+++ src/components/dice/dice.js
@@ -5,7 +5,7 @@ export const Dice = props => {
     return <button onClick={() => props.markToRoll(props.dice_props.id)} 
                     className={`dice ${props.dice_props.roll ? 'diceToRoll' : ''}`}
                     id={props.dice_props.id}>
-                {props.dice_props.number}
+                {props.dice_props.value}
             </button>
 }
 
--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,13 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
 import './game.css'
-import Table from '../table/table'
+import Dice from '../dice/dice'
 
-export default class Game extends React.Component {
+export default class Game extends Component {
+    state = {
+        currentPlayer: null,
+        indexOfFirstPlayer: null,
+        isActive: true,
+        mug:[{
+                id: 0,
+                value: null,
+                roll: false
+            },{
+                id: 1,
+                value: null,
+                roll: false
+            },{
+                id: 2,
+                value: null,
+                roll: false
+            },{
+                id: 3,
+                value: null,
+                roll: false
+            },{ 
+                id: 4,
+                value: null,
+                roll: false
+            }
+        ],
+        numberOfRoll: null,
+        numberOfTurn: null,
+        playerIDs: null,
+        players: null,
+        id: null
+    }
+
+    async getGame() {
+        const response = await fetch('http://127.0.0.1:3001/api/v1/user/1/game/624769a7d673c4335ea6f2d2')
+        let body = await response.text();
+        body = JSON.parse(body);
+        this.setState({ id: body._id });
+        const game = body.game
+
+        this.setState({ currentPlayer: game.currentPlayer });
+        this.setState({ indexOfFirstPlayer: game.indexOfFirstPlayer });
+        this.setState({ isActive: game.isActive });
+        for (const [id, value] of Object.entries(game.mug)) {
+            const dice = this.state.mug.find(dice => dice.id == id)
+            dice.value = value
+        }
+        this.setState({ numberOfRoll: game.numberOfRoll });
+        this.setState({ numberOfTurn: game.numberOfTurn });
+        this.setState({ playerIDs: game.playerIDs });
+        this.setState({ players: game.players });
+        
+    }
+
+    componentDidMount() {
+        this.getGame()
+    }
+
+    markToRoll(id) {
+        const dice = this.state.mug.find(dice => dice.id == id)
+        dice.roll = !dice.roll
+        this.setState( {game: this.state.mug})
+    }
+
     render() {
+        const mug = this.state.mug.map(dice => <Dice key={dice.id} dice_props={dice} markToRoll={this.markToRoll.bind(this)}/> ) 
         return (
-            <div className="game">
-                <Table/>
+            <div className="table">
+                <p>Player: {this.state.currentPlayer}</p> 
+                {mug}
             </div>
         )
+        ;
     }
 }
