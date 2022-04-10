1818b2f160b72169f86747ba3dfb9e3cd486a938
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-04-10 20:41:37 +0200

    Added enable and disable buttons

--- src/components/alerts/ErrorReqest.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Alert } from 'react-bootstrap';
-
-export const ErrorReqest = props => {
-    return <Alert variant="danger">{props.elems.error}</Alert>
-}
-
-export default ErrorReqest
--- src/components/alerts/Tip.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Alert } from 'react-bootstrap';
-
-export const Tip = props => {
-    return <Alert variant="warning">{props.elems.tip}</Alert>
-}
-
-export default Tip
