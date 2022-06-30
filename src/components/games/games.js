80bae25bfc9b09f906df97991414e7e6bd54805a
Bartosz Lenartowicz <bartek.lenart95@gmail.com>
2022-06-30 17:40:14 +0200

    Fix display games

--- src/components/games/games.js
+++ src/components/games/games.js
@@ -5,32 +5,27 @@ import GameInfo from '../gameInfo/gameInfo'
 
 const Games = props => {
 
-  const getGames = () => {
-    console.log(props)
-    const requestOptions = {
-      headers: {
-          'Authorization': `Bearer ${props.keycloak.token}`
-      },
-      mode: 'no-cors',
-    };
+  const [games, setGames] = useState([])
 
-    console.log(requestOptions)
-
-    fetch(`http://localhost:3000/api/v1/user/1/game/6269a20f9ac7b2241521cd39`, requestOptions)
-    .then(response => console.log(response))
-    .then(responseData => {console.log(responseData)})
-    .catch(err => {console.log(err) })
+  const getGames = async () => {
+    if(props.keycloak.authenticated) {
+      const requestOptions = {
+        headers: {
+            'Authorization': `Bearer ${props.keycloak.token}`
+        }
+      };
+      const response = await fetch(`${process.env.REACT_APP_DICE_GAME_API}/user/1/game`, requestOptions)
+      const body = await response.json()
+      console.log(body)
+      setGames(body)
+    }
   }
 
-  useEffect(() => {getGames()}, [])
-  //this.getGames()
-  // const games = this.state.games.map(game => <GameInfo game={game} />)
-  // return <Container>
-  //   <Accordion defaultActiveKey="0">
-  //     {games}
-  //   </Accordion>
-  // </Container>
-  return <div>Games</div>
+  useEffect(() => { getGames() })
+
+  return <Container>
+    { games.map(game => <GameInfo game={game} />) }
+  </Container>
 }
 
 export default Games
